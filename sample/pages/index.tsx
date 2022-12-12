75d802ba220b30a18099af19831aea5eef9305a4
Frank A. Krueger <fak@praeclarum.org>
2022-12-11 18:31:50 -0800

    Send messages to and from the web worker

--- sample/pages/index.tsx
+++ sample/pages/index.tsx
@@ -4,11 +4,12 @@ import styles from '../styles/Home.module.css'
 
 import { AutoTokenizer, AutoModelForSeq2SeqLM } from '../../lib';
 
-import { useState, useRef, FormEvent, ChangeEvent, useEffect, useMemo } from 'react';
+import { useState, useRef, useCallback, FormEvent, ChangeEvent, useEffect, useMemo } from 'react';
 
 export default function Home() {
 
   const modelId = "t5-small";
+  const modelsPath = "/models";
 
   const [status, setStatus] = useState('Loading...');
   const [inputLang, setInputLang] = useState('English');
@@ -17,8 +18,8 @@ export default function Home() {
   const [inputDebug, setInputDebug] = useState('');
   const [outputText, setOutputText] = useState('');
 
-  const tokenizer = useRef(AutoTokenizer.fromPretrained(modelId, "/models"));
-  const model = useRef(AutoModelForSeq2SeqLM.fromPretrained(modelId, "/models", async function (progress) {
+  const tokenizer = useRef(AutoTokenizer.fromPretrained(modelId, modelsPath));
+  const model = useRef(AutoModelForSeq2SeqLM.fromPretrained(modelId, modelsPath, async function (progress) {
     const message = `Loading the neural network... ${Math.round(progress * 100)}%`;
     setStatus(message);
     setOutputText(message);
@@ -36,7 +37,7 @@ export default function Home() {
     translate(inputText, newOutputLang);
   }
 
-  const translate = async (inputText: string, outputLang: string) => {
+  const translateWithModel = async (inputText: string, outputLang: string) => {
     const generationOptions = {
         "maxLength": 50,
         "topK": 0,
@@ -54,24 +55,35 @@ export default function Home() {
     setStatus("Ready");
   };
 
-  const worker = useMemo(() => typeof window !== "undefined" ? new Worker("/worker.js") : null, []);
+  const workerRef = useRef<Worker>();
+  useEffect(() => {
+    workerRef.current = new Worker(new URL('../worker.ts', import.meta.url))
+    workerRef.current.onmessage = (event: MessageEvent<number>) =>
+      setOutputText(event.data+"");
+    return () => {
+      if (workerRef.current)
+        workerRef.current.terminate();
+    }
+  }, []);
+  // const handleWork = useCallback(async () => {
+  //   if (workerRef.current)
+  //       workerRef.current.postMessage(100000);
+  // }, []);
 
-  const translateWithWorker = async (inputText: string, outputLang: string) => {
-    const generationOptions = {
+  const translate = async (inputText: string, outputLang: string) => {
+    const fullInput = `translate ${inputLang} from English to ${outputLang}: ${inputText.trim()}`;
+    const command = {
+      "command": "generate",
+      "input": fullInput,
+      "modelId": modelId,
+      "modelsPath": modelsPath,
+      "generationOptions": {
         "maxLength": 50,
         "topK": 0,
+      }
     };
-    const fullInput = `translate ${inputLang} to ${outputLang}: ${inputText.trim()}`;
-    const inputTokenIds = await tokenizer.current.encode(fullInput);
-    async function generateProgress(outputTokenIds: number[], forInputIds: number[]) {
-        let shouldContinue = true;
-        return shouldContinue;
-    }
-    const finalOutputTokenIds = await model.current.generate(inputTokenIds, generationOptions, generateProgress);
-    const finalOutput = (await tokenizer.current.decode(finalOutputTokenIds, true)).trim();
-    setInputDebug(fullInput + " = " + inputTokenIds.join(" "));
-    setOutputText(finalOutput);
-    setStatus("Ready");
+    if (workerRef.current)
+        workerRef.current.postMessage(command);
   };
 
   return (
--- sample/worker.ts
+++ sample/worker.ts
@@ -1,3 +1,25 @@
 import { AutoTokenizer, AutoModelForSeq2SeqLM } from '../lib';
 
+let modelId = "t5-small";
+const tokenizer = AutoTokenizer.fromPretrained(modelId, "/models");
+const translate = async (inputText: string, inputLang: string, outputLang: string) => {
+    console.log(`Translating {inputText} from {inputLang} to {outputLang}`);
+    const generationOptions = {
+        "maxLength": 50,
+        "topK": 0,
+    };
+    const fullInput = `translate ${inputLang} to ${outputLang}: ${inputText.trim()}`;
+    const inputTokenIds = await tokenizer.encode(fullInput);
+    console.log(inputTokenIds);
+}
+
 console.log("Hello from worker.ts");
+translate("Hello world", "English", "French");
+
+onmessage = (e) => {
+    const command = e.data;
+    console.log('Message received from main script', command);
+    const workerResult = command.input + "!!!";
+    console.log('Posting message back to main script');
+    postMessage(workerResult);
+}

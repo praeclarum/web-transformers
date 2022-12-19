d23e6a13113c1b20463ca8f7724c9db7cc622007
Frank A. Krueger <fak@praeclarum.org>
2022-12-19 07:05:52 -0800

    Reify generation requests

--- README.md
+++ README.md
@@ -4,6 +4,23 @@
 
 Transformer neural networks in the browser.
 
+## Sample
+
+This repo contains a sample nextjs app that translates text using a transformer neural network.
+
+Before you can run the sample, you need to download the model files. You can do this by running the following command:
+
+```bash
+cd sample
+npm run download_models
+```
+
+Then you can run the sample with:
+
+```bash
+npm run dev
+```
+
 ## License
 
 Copyright © 2022 [Frank A. Krueger](https://github.com/praeclarum).<br />
--- sample/worker.ts
+++ sample/worker.ts
@@ -1,9 +1,5 @@
 import { AutoTokenizer, AutoModelForSeq2SeqLM } from '../lib';
 
-let requestedInputText = "";
-let requestedModelId = "t5-small";
-let requestedModelsPath = "/models";
-
 class ModelData {
     modelId: string;
     modelsPath: string;
@@ -17,33 +13,65 @@ class ModelData {
     }
 }
 
-const generate = async () => {
-    const inputText = requestedInputText;
-    const model = new ModelData(requestedModelId, requestedModelsPath);
-    console.log(`Translating {inputText} from {inputLang} to {outputLang}`);
-    const generationOptions = {
-        "maxLength": 50,
-        "topK": 0,
-    };
+class GenerationRequest {
+    input: string = "";
+    maxLength: number = 50;
+    topK: number | undefined;
+    modelId: string;
+    modelsPath: string;
+    constructor(modelId: string, modelsPath: string) {
+        this.modelId = modelId;
+        this.modelsPath = modelsPath;
+    }
+    equals(other: GenerationRequest) {
+        return this.input === other.input &&
+            this.modelId === other.modelId &&
+            this.modelsPath === other.modelsPath &&
+            this.maxLength === other.maxLength &&
+            this.topK === other.topK;
+    }
+}
+
+let lastRequest: GenerationRequest | null = null;
+let executingRequest: GenerationRequest | null = null;
+
+let executingModelData: ModelData | null = null;
+
+const generate = async (request: GenerationRequest) => {
+    const model = new ModelData(request.modelId, request.modelsPath);
+    executingModelData = model;
+    function shouldContinue(): boolean {
+        return executingRequest !== null && request.equals(executingRequest);
+    }
+    console.log(`Generating from input '{request.input}' with model '{request.modelId}'`);
+    const inputText = request.input;
     const inputTokenIds = await model.tokenizer.encode(inputText);
-    console.log(inputTokenIds);
+    console.log(`Generating from tokens: {inputTokenIds}`);
+    const generationOptions = {
+        "maxLength": request.maxLength,
+        "topK": request.topK,
+    };
     async function generateProgress(outputTokenIds: number[], forInputIds: number[]) {
-        let shouldContinue = true;
-        return shouldContinue;
+        if (shouldContinue()) {
+            const outputText = (await model.tokenizer.decode(outputTokenIds, true)).trim();
+            postMessage({"inputText": inputText, "outputText": outputText, "complete": false});
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     const finalOutputTokenIds = await model.model.generate(inputTokenIds, generationOptions, generateProgress);
     const finalOutput = (await model.tokenizer.decode(finalOutputTokenIds, true)).trim();
-    postMessage(finalOutput);
+    postMessage({"inputText": inputText, "outputText": finalOutput, "complete": true});
 }
 
 console.log("Hello from worker.ts");
 // translate("Hello world", "English", "French");
 
 onmessage = (e) => {
-    const command = e.data;
-    console.log('Message received from main script', command);
-    const workerResult = command.input + "!!!";
-    requestedInputText = command.input;
-    console.log('Posting message back to main script');
-    generate();    
+    const request = e.data;
+    lastRequest = request;
+    console.log('Message received from main script:', request);
+    generate(request);    
 }

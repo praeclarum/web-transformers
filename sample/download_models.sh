e40bde22261857bb8b3313e5797f9cbd10fb7b70
Frank A. Krueger <fak@praeclarum.org>
2022-12-21 13:17:37 -0800

    Download tokenizer with model

--- sample/download_models.sh
+++ sample/download_models.sh
@@ -5,6 +5,7 @@ MODEL_DIR=public/models
 
 mkdir -p $MODEL_DIR
 
+curl https://transformers-js.praeclarum.org/models/$MODEL_ID-tokenizer.json --output $MODEL_DIR/$MODEL_ID-tokenizer.json
 curl https://transformers-js.praeclarum.org/models/$MODEL_ID-encoder-quantized.onnx --output $MODEL_DIR/$MODEL_ID-encoder-quantized.onnx
 curl https://transformers-js.praeclarum.org/models/$MODEL_ID-init-decoder-quantized.onnx --output $MODEL_DIR/$MODEL_ID-init-decoder-quantized.onnx
 curl https://transformers-js.praeclarum.org/models/$MODEL_ID-decoder-quantized.onnx --output $MODEL_DIR/$MODEL_ID-decoder-quantized.onnx
